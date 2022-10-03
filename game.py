75e6435862cd1b3634f29e612148acff1721f638
Wachira2000 <lewiswachira996@gmail.com>
2022-10-03 17:07:15 +0300

    Task commit game

---
+++ game.py
@@ -0,0 +1,33 @@
+import random
+
+def get_choices():
+  player_choice = input("enter a choice (rock, paper, scissors:) ")
+  options = ["rock", "paper", "scissors"]
+  computer_choice = random.choice(options)
+  choices = {"player": player_choice, "computer": computer_choice}
+  return choices
+
+def check_win(player, computer):
+  print(f"You chose {player}, computer chose {computer}")
+  if player == computer:
+    return "It's a tie!"
+  elif player == "rock": 
+    if computer == "scissors":
+     return "rock smashes scissors! You Win!"
+    else:  
+     return "paper covers rock! You lose."
+  elif player == "paper": 
+    if computer == "rock":
+     return "paper covers rock! You Win!"
+    else:
+     return "scissors cut paper! You lose."
+  elif player == "scissors": 
+    if computer == "paper":
+     return "scissors cut paper! You Win!"
+    else:
+     return "rock smashes scissors! You lose."
+   
+
+choices = get_choices()
+result = check_win(choices["player"], choices["computer"])
+print(result)
